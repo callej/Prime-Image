6b7338eb160576d67ede27026cd3746ae7d24a27
Calle Johansson <callej@live.com>
2023-04-24 10:36:16 -0700

    A first functioning version of the primeImage program. The program creates an image with the provided size and the background color provided, and where the prime numbers are marked with the provided color for the prime numbers. The upper left corner is 1. Then the numbers increase column by column and then row by row, in the same way as this text is read.
    The resulting image is stored in a file named PrimeImage.png

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
-data class Color(var r: Int, var g: Int, var b: Int)
+import java.awt.image.BufferedImage
+import java.awt.Color
+import java.io.File
+import javax.imageio.ImageIO
 
 fun inputColor(msg: String): Color {
     println(msg)
@@ -8,9 +11,42 @@ fun inputColor(msg: String): Color {
     return Color(red, green, blue)
 }
 
+fun inputDimentions(msg: String): Pair<Int, Int> {
+    return print(msg).run { readln().split(",").map { it.trim().toInt() } }.run { Pair(this[0], this[1]) }
+}
+
+fun primeList(limit: Int): List<Int> {
+    val primes = mutableListOf<Int>()
+    for (testNumber in 2..limit) {
+        var isPrime = true
+        for (prime in primes) {
+            if (prime * prime > testNumber) break
+            if (testNumber % prime == 0) {
+                isPrime = false
+                break
+            }
+        }
+        if (isPrime) {
+            primes.add(testNumber)
+        }
+    }
+    return primes
+}
+
 fun main() {
     val bgColor = inputColor("Input the background color (RGB)")
     val primeColor = inputColor("Input the color for the prime numbers (RGB)")
-    println(bgColor)
-    println(primeColor)
+    val (width, height) = inputDimentions("Input the image dimensions in pixels (width, height): ")
+    val primes = primeList(width * height)
+    val image = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+    for (y in 0 until image.height) {
+        for (x in 0 until image.width) {
+            if (x + 1 + width * y in primes) {
+                image.setRGB(x, y, primeColor.rgb)
+            } else {
+                image.setRGB(x, y, bgColor.rgb)
+            }
+        }
+    }
+    ImageIO.write(image, "png", File("PrimeImage.png"))
 }
